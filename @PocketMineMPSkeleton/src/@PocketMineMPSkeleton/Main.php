2b3f1f2d7772f99332d59c4958cf14c8eece3d2e
Michael Davis <michael@daviskingdom.com>
2015-03-19 21:55:49 +0100

    Initial Commit

---
+++ @PocketMineMPSkeleton/src/@PocketMineMPSkeleton/Main.php
@@ -0,0 +1,20 @@
+<?php
+ 
+namespace Example;
+ 
+use pocketmine\plugin\PluginBase;
+ 
+class Main extends PluginBase{
+ 
+        public function onLoad(){
+                $this->getLogger()->info("onLoad() has been called!");
+        }
+ 
+        public function onEnable(){
+                $this->getLogger()->info("onEnable() has been called!");
+        }
+ 
+	
+		
+	}
+}
