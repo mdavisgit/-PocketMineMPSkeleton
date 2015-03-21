70961b981dc90d2f732e1524668f254e2dd1b874
Michael Davis <michael@daviskingdom.com>
2015-03-21 17:14:20 +0100

    Adding code for writing custom events

--- @PocketMineMPSkeleton/src/@PocketMineMPSkeleton/Main.php
+++ @PocketMineMPSkeleton/src/@PocketMineMPSkeleton/Main.php
@@ -3,6 +3,7 @@
 namespace Example;
  
 use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerJoinEvent;
  
 class Main extends PluginBase{
  
@@ -12,6 +13,11 @@ class Main extends PluginBase{
  
         public function onEnable(){
                 $this->getLogger()->info("onEnable() has been called!");
+                $this->getServer()->getPluginManager()->registerEvents($this,$this);
+                //This line above is required to use additional events
+                // Also must include "use pocketmine\event\player\<EventNameToUse>"
+                //  You can get supported event names here: http://docs.pocketmine.net/d3/daf/namespacepocketmine_1_1event.html
+                //   expand the events on the left column to see the choices.  Ex. "use pocketmine\event\player\PlayerJoinEvent"
         }
  
 		public function onDisable() {
